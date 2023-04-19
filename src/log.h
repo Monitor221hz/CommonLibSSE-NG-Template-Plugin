25d93016315b59c864d653d0f14be84637e5203f
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2023-04-19 17:35:37 +0100

    first commit

---
+++ src/log.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <spdlog/sinks/basic_file_sink.h>
+
+void SetupLog() {
+    auto logsFolder = SKSE::log::log_directory();
+    if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
+    auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
+    auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
+    auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
+    auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+    spdlog::set_default_logger(std::move(loggerPtr));
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::flush_on(spdlog::level::trace);
+}
---
+++ src/plugin.cpp
@@ -0,0 +1,31 @@
+#include "log.h"
+
+
+void OnDataLoaded()
+{
+   
+}
+
+void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+{
+	switch (a_msg->type) {
+	case SKSE::MessagingInterface::kDataLoaded:
+        
+		break;
+	case SKSE::MessagingInterface::kPostLoad:
+		break;
+	case SKSE::MessagingInterface::kPreLoadGame:
+		break;
+	case SKSE::MessagingInterface::kPostLoadGame:
+        break;
+	case SKSE::MessagingInterface::kNewGame:
+		break;
+	}
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+    SKSE::Init(skse);
+	SetupLog();
+    
+    return true;
+}
