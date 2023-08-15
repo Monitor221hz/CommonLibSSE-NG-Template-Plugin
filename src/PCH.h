ff7aa7a05bd9919c76eb260dfe5907f861533ee6
Screen <monitor.144.hz.100.srgb@gmail.com>
2023-08-15 18:29:35 +0100

    added po3's stl utils

--- src/PCH.h
+++ src/PCH.h
@@ -4,3 +4,30 @@
 #include "SKSE/SKSE.h"
 
 using namespace std::literals;
+
+namespace stl
+{
+	using namespace SKSE::stl;
+
+	template <class F, size_t index, class T>
+	void write_vfunc()
+	{
+		REL::Relocation<std::uintptr_t> vtbl{ F::VTABLE[index] };
+		T::func = vtbl.write_vfunc(T::size, T::thunk);
+	}
+
+	template <class F, class T>
+	void write_vfunc()
+	{
+		write_vfunc<F, 0, T>();
+	}
+
+	template <class T>
+	void write_thunk_call(std::uintptr_t a_src)
+	{
+		auto& trampoline = SKSE::GetTrampoline();
+		SKSE::AllocTrampoline(14);
+
+		T::func = trampoline.write_call<5>(a_src, T::thunk);
+	}
+}
